0343fcef090364841b70cf011dde1dab1a538e66
Khalid kakar <126089249+khalidkhankakar@users.noreply.github.com>
2024-02-20 15:10:56 +0500

    Update page.js

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,7 +1,7 @@
 
 import { connectToDatabase } from "@/lib/database/connect";
 export default async function Home() {
-  const connected = await connectToDatabase()
+
 
   return (
     <div className="">
